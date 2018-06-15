10e852951df3dd76f01339b2e07a4e9db3fe42e4
Akim Desire <adesi006.ng@gmail.com>
2018-06-15 11:52:47 -0400

    added thoughts on user stories to pseudocode section

---
+++ w1D5-Stubbing.js
@@ -0,0 +1,11 @@
+/*
+User stories to function stubs
+
+User stories are a description of what the app is supposed to do, told from a
+user's perspective.
+
+Pseudocode is a description of what must be done to achieve each piece of
+functionality.
+
+Function stubs usually contain pseudocode which describes what the function will do.
+*/
