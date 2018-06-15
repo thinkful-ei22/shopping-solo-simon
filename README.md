3e2d8e9a85a1f532c3a9c13bc38c58df6a66a95e
Simon Chae <s13chae@gmail.com>
2018-06-15 11:15:51 -0700

    first commit to new solo shopping list

---
+++ README.md
@@ -0,0 +1,1 @@
+Extend shopping list app SOLO;
--- w1D5-Stubbing.js
+++ w1D5-Stubbing.js
@@ -9,3 +9,9 @@ functionality.
 
 Function stubs usually contain pseudocode which describes what the function will do.
 */
+
+/* 
+
+Created values in our STORE variable. The values inputted here will be the default values shown after running our program. 
+
+Store represent the current status of our DOM.
